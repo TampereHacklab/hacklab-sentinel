f602a4cb747f4750144f372da5a701630b1156f6
helgrima <arttu.tamminen@kapsi.fi>
2017-06-06 23:28:45 +0300

    some end points to generate data and retrieve data. will be deleted in future

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var models  = require('../models');
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
@@ -32,4 +33,60 @@ router.get("/secret", function(req, res, next) {
 
 });
 
+router.get("/dctest", function(req, res) {
+    models.DataCollector.findAll({
+        include: [{model: models.Input, as: "inputs"}]
+    }).then(function(dcs) {
+        res.json(dcs);
+    })
+});
+router.get("/inputtest", function(req, res) {
+    models.Input.findAll({
+        include:  [{model: models.DataCollector, as: "dataCollector"}]
+    }).then(function(dcs) {
+        res.json(dcs);
+    })
+});
+
+router.get("/generate", function(req, res) {
+    var newdc = {
+        name: "Data collector 1",
+        description: "First data collector",
+        identifier: "123456789",
+        inputs: []
+
+    };
+    for(var i = 0; i < 7; i++) {
+        newdc.inputs.push({
+            index: i,
+            fallingDelay: 0,
+            risingDelay: 0,
+            resolution: 1000,
+            enabled: true,
+            device: {
+                name: "Device " + (i + 1),
+                description: "Data producer " + (i + 1)
+            }
+        });
+    }
+
+    models.DataCollector.create(newdc,
+        {
+            include: [{
+                    model: models.Input,
+                    as: "inputs",
+                    include: [{
+                        model: models.Device,
+                        as: "device"
+                    }]
+                },
+
+
+            ]
+        }
+    ).then(function(dc) {
+
+        res.json(dc);
+    })
+});
 module.exports = router;

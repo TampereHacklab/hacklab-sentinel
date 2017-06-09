c1eab4f1aee3e1d30656eb16f7df5d2570e6a3db
helgrima <arttu.tamminen@kapsi.fi>
2017-06-09 18:43:47 +0300

    enhanced realtime

--- public/javascripts/realtime.js
+++ public/javascripts/realtime.js
@@ -78,19 +78,24 @@ function parseClientMessages(topic, message) {
         };
 
         var c = $("<div></div>").addClass("underling");
-        var h = $("<h1></h1>").html(clients[client].name).addClass("header");
+        var hw = $("<div></div>").addClass("header-wrapper").css("background-color", jsonMessage.color);
+        var h = $("<h1></h1>").html(clients[client].name).addClass("header").css("color", isColorLight(jsonMessage.color) ? "black" : "white");
+        var cw = $("<div></div>").addClass("content-wrapper");
         var m = $("<span></span>").addClass("message");
         var t = $("<span></span>").addClass("timestamp");
         var d = $("<span></span>").addClass("duration");
-        var s = $("<div></div>").addClass("state").css("background-color", jsonMessage.color);
         $(m).html(clients[client].message);
         $(t).html(clients[client].seen.format("HH:mm:ss DD.MM.YYYY"));
-        $(c).append(h);
-        $(c).append(m);
-        $(c).append(t);
-        $(c).append(d);
-        $(c).append(s);
+        $(hw).append(h);
+        $(c).append(hw);
+
+        $(cw).append(m);
+        $(cw).append(t);
+        $(cw).append(d);
+        $(c).append(cw);
         clients[client].container = c;
+        var duration = moment().diff(clients[client].seen);
+        $(clients[client].container).find(".duration").html(moment.duration(duration).humanize());
         clients[client].durationInterval = setInterval(function() {
             var duration = moment().diff(clients[client].seen);
             $(clients[client].container).find(".duration").html(moment.duration(duration).humanize());
@@ -103,9 +108,27 @@ function parseClientMessages(topic, message) {
         clients[client].seen =  moment(jsonMessage.timestamp, "YYYY-MM-DD[T]HH:mm:ss.sss"),
         clients[client].status = "OK";
         clients[client].message = jsonMessage.state,
-        $(clients[client].container).find(".header").html(clients[client].name);
+        $(clients[client].container).find(".header-wrapper").css("background-color", jsonMessage.color);
+        $(clients[client].container).find(".header").html(clients[client].name).css("color", isColorLight(jsonMessage.color) ? "black" : "white");
         $(clients[client].container).find(".message").html(clients[client].message);
         $(clients[client].container).find(".timestamp").html(clients[client].seen.format("HH:mm:ss DD.MM.YYYY"));
         $(clients[client].container).find(".state").css("background-color", jsonMessage.color);
     }
+}
+
+//https://codepen.io/anon/pen/vZLReL
+function isColorLight(hex) {
+    var rgb = hexToRgb(hex);
+    var a = 1 - (0.299 * rgb.r + 0.587 * rgb.g + 0.114 * rgb.b) / 255;
+    return (a < 0.5);
+}
+
+//https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
 }

54957fcccd81ecd9054bc8e19989ee65d5e6da94
helgrima <arttu.tamminen@kapsi.fi>
2017-06-13 20:24:09 +0300

    refactoring realtime

--- public/javascripts/realtime.js
+++ public/javascripts/realtime.js
@@ -5,6 +5,10 @@ client.on("connect", function() {
     client.publish("hacklab/tampere/realtime/request/DC1");
 });
 
+$(window).focus(function() {
+    client.publish("hacklab/tampere/realtime/request/DC1");
+});
+
 client.on("message", function(topic, message) {
     var msg = message.toString();
     if(topic.indexOf("realtime") > 0) {
@@ -13,70 +17,26 @@ client.on("message", function(topic, message) {
 });
 
 function parseClientMessages(topic, message) {
-    var client = topic.split("/")[4];
+    var identifier = topic.split("/")[4];
 	var jsonMessage = JSON.parse(message.toString())
-    if(typeof clients[client] == "undefined") {
-        clients[client] = {
+    if(typeof clients[identifier] == "undefined") {
+        clients[identifier] = new Underling({
+            container: ".underling-container",
+            seen: jsonMessage.timestamp,
             name: jsonMessage.device,
-            seen: moment(jsonMessage.timestamp, "YYYY-MM-DD[T]HH:mm:ss.sss"),
-            status: "OK",
-            message: jsonMessage.state,
-            container: null,
-            durationInterval: null,
-        };
+            state: jsonMessage.state,
+            identifier: identifier,
+            color: jsonMessage.color
+        });
 
-        var c = $("<div></div>").addClass("underling");
-        var hw = $("<div></div>").addClass("header-wrapper").css("background-color", jsonMessage.color);
-        var h = $("<h1></h1>").html(clients[client].name).addClass("header").css("color", isColorLight(jsonMessage.color) ? "black" : "white");
-        var cw = $("<div></div>").addClass("content-wrapper");
-        var m = $("<span></span>").addClass("message");
-        var t = $("<span></span>").addClass("timestamp");
-        var d = $("<span></span>").addClass("duration");
-        $(m).html(clients[client].message);
-        $(t).html(clients[client].seen.format("HH:mm:ss DD.MM.YYYY"));
-        $(hw).append(h);
-        $(c).append(hw);
-
-        $(cw).append(m);
-        $(cw).append(t);
-        $(cw).append(d);
-        $(c).append(cw);
-        clients[client].container = c;
-        var duration = moment().diff(clients[client].seen);
-        $(clients[client].container).find(".duration").html(moment.duration(duration).humanize());
-        clients[client].durationInterval = setInterval(function() {
-            var duration = moment().diff(clients[client].seen);
-            $(clients[client].container).find(".duration").html(moment.duration(duration).humanize());
-        }, 1000);
-
-        $(".underling-container").append(c);
     }
     else {
-		clients[client].name = jsonMessage.device;
-        clients[client].seen =  moment(jsonMessage.timestamp, "YYYY-MM-DD[T]HH:mm:ss.sss"),
-        clients[client].status = "OK";
-        clients[client].message = jsonMessage.state,
-        $(clients[client].container).find(".header-wrapper").css("background-color", jsonMessage.color);
-        $(clients[client].container).find(".header").html(clients[client].name).css("color", isColorLight(jsonMessage.color) ? "black" : "white");
-        $(clients[client].container).find(".message").html(clients[client].message);
-        $(clients[client].container).find(".timestamp").html(clients[client].seen.format("HH:mm:ss DD.MM.YYYY"));
-        $(clients[client].container).find(".state").css("background-color", jsonMessage.color);
+        clients[identifier].refresh({
+            seen: jsonMessage.timestamp,
+            name: jsonMessage.device,
+            state: jsonMessage.state,
+            color: jsonMessage.color
+        });
+        clients[identifier].update();
     }
-}
-
-//https://codepen.io/anon/pen/vZLReL
-function isColorLight(hex) {
-    var rgb = hexToRgb(hex);
-    var a = 1 - (0.299 * rgb.r + 0.587 * rgb.g + 0.114 * rgb.b) / 255;
-    return (a < 0.5);
-}
-
-//https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-function hexToRgb(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
 }
---
+++ public/javascripts/underling.js
@@ -0,0 +1,47 @@
+function Underling(options) {
+    var that = this;
+    this.refresh = function(options) {
+        that.identifier = options.identifier;
+        that.name = options.name;
+        that.seen = moment(options.seen, "YYYY-MM-DD[T]HH:mm:ss.sss");
+        that.state = options.state;
+        that.color = options.color;
+    };
+
+    this.update = function() {
+        that.elements.headerWrapper.css("background-color", that.color);
+        that.elements.header.html(this.name).css("color", utilities.isColorLight(that.color) ? "black" : "white");
+        that.elements.content.html(this.state);
+        that.elements.timestamp.html(this.seen.format("HH:mm:ss DD.MM.YYYY"));
+    };
+
+    this.updateDuration = function() {
+        var duration = Math.round(moment().diff(that.seen) / 1000);
+        that.elements.duration.html(utilities.toDurationStr(duration));
+    };
+
+    this.refresh(options);
+
+    this.elements = {};
+    this.elements.container =  $("<div></div>").addClass("underling");
+    this.elements.headerWrapper = $("<div></div>").addClass("header-wrapper");
+    this.elements.header = $("<h1></h1>").addClass("header");
+    this.elements.contentWrapper = $("<div></div>").addClass("content-wrapper");
+    this.elements.content = $("<div></div>").addClass("content");
+    this.elements.timestamp = $("<div></div>").addClass("timestamp");
+    this.elements.duration = $("<div></div>").addClass("duration");
+
+    this.elements.headerWrapper.append(this.elements.header);
+
+    this.elements.contentWrapper.append(this.elements.content);
+    this.elements.contentWrapper.append(this.elements.timestamp);
+    this.elements.contentWrapper.append(this.elements.duration);
+
+    this.elements.container.append(this.elements.headerWrapper);
+    this.elements.container.append(this.elements.contentWrapper);
+
+    this.update(options);
+    this.updateDuration();
+    setInterval(this.updateDuration, 1000);
+    $(options.container).append(this.elements.container);
+}

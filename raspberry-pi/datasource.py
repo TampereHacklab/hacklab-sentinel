0ef6e190047a086406fc0c96f3d98df62a4f3f77
helgrima <arttu.tamminen@kapsi.fi>
2017-06-16 19:08:55 +0300

    python script which polls inputs instead of using interrups, as they do not work for unknonw reason

--- raspberry-pi/datasource.py
+++ raspberry-pi/datasource.py
@@ -4,10 +4,35 @@ import sys
 import json
 import hashlib
 import configparser
-#import pifacedigitalio
+import pifacedigitalio
+from threading import Timer
+
+NUMBER_OF_INPUTS = 8
+DELAY_BETWEEN_READS = 1
 
 config = configparser.ConfigParser()
 config.read("config.ini")
+inputs = [None] * NUMBER_OF_INPUTS
+
+
+
+class Input:
+	resolution = 0
+	lastValue = None
+	lastTime = None
+	
+	def isNew(self, currentValue, currentTime):
+		if self.lastValue == None:
+			self.lastValue = currentValue
+			self.lastTime = currentTime
+			return True
+		elif self.lastValue != currentValue:
+			self.lastValue = currentValue
+			self.lastTime = currentTime
+			return True
+		else:
+			return False
+
 def input_change(event):
 	print(event)
 	state = "low"
@@ -20,25 +45,52 @@ def input_change(event):
 	client.publish(topic, message)
 
 def on_connect(client, userdata, flags, rc):
-	print("Connected with result code "+str(rc))
-	hwindex = sys.argv[1]
-	state = str(sys.argv[2])
-	timestamp = datetime.now().isoformat()
-	topic = config["mqtt"]["baseTopic"] + "/tampere/datagate/" + config["messages"]["identifier"] + "/" + str(hwindex)
+	print("Connected to broker with result code "+str(rc))
+	#hwindex = sys.argv[1]
+	#state = str(sys.argv[2])
+	#timestamp = datetime.now().isoformat()
 
-	print(topic)
+	#print(topic)
 
+	#checksum = hashlib.md5(state.encode("utf-8") + timestamp.encode("utf-8") + config["messages"]["salt"].encode("utf-8"))
+	#message = json.dumps({"state": state, "timestamp": timestamp, "checksum": checksum.hexdigest()})
+	#print(message)
+	#client.publish(topic, message)
+	#client.disconnect()
+
+def readInputs():
+	#print("Read inputs")   
+	current = datetime.now()
+	for i in range(0, NUMBER_OF_INPUTS - 1):
+		v = pfd.input_pins[i].value             
+		if(inputs[i].isNew(v, current)):
+			print("Iput change: " + current.isoformat())
+			print(str(i) + ": " +  str(v))
+			sendData(i, v, current)
+	Timer(DELAY_BETWEEN_READS, readInputs).start()
+
+
+def sendData(input, dir, time):
+	state = "low"
+	if dir == 1:
+		state = "high"
+	
+	timestamp = time.isoformat()
+	topic = config["mqtt"]["baseTopic"] + "/tampere/datagate/" + config["messages"]["identifier"] + "/" + str(input)
 	checksum = hashlib.md5(state.encode("utf-8") + timestamp.encode("utf-8") + config["messages"]["salt"].encode("utf-8"))
 	message = json.dumps({"state": state, "timestamp": timestamp, "checksum": checksum.hexdigest()})
-	print(message)
 	client.publish(topic, message)
-	client.disconnect()
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
 
 print("Starting")
-print(config["mqtt"]["broker"])
+for i in range(0, NUMBER_OF_INPUTS - 1):
+	inputs[i] = Input()
+
+
+print("Connecting to broker")
 client.connect(config["mqtt"]["broker"], int(config["mqtt"]["port"]), int(config["mqtt"]["timeout"]))
 
 # Blocking call that processes network traffic, dispatches callbacks and
@@ -46,23 +98,12 @@ client.connect(config["mqtt"]["broker"], int(config["mqtt"]["port"]), int(config
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
 
+print("Starting piface")
 
-'''
 pfd = pifacedigitalio.PiFaceDigital()
 print("piface created")
-print(pfd.input_pins[0].value)
-print(pfd.input_pins[1].value)
-print(pfd.input_pins[2].value)
-print(pfd.input_pins[3].value)
-print(pfd.input_pins[4].value)
-print(pfd.input_pins[5].value)
-print(pfd.input_pins[6].value)
-print(pfd.input_pins[7].value)
-listener = pifacedigitalio.InputEventListener(chip=pfd)
-for i in range(0, 8):
-	listener.register(i, pifacedigitalio.IODIR_FALLING_EDGE, input_change)
-	listener.register(i, pifacedigitalio.IODIR_RISING_EDGE, input_change)
 
-listener.activate()
-'''
+Timer(DELAY_BETWEEN_READS, readInputs).start()
+
 client.loop_forever()
+
